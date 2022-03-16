3ae044c33343017f14725430adcd2f233dca438f
ANIL KUMAR <anilkumarmr@protonmail.com>
2022-03-16 16:55:15 +0530

    added dev env

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import CreateExpense from "./components/CreateExpense";
 import EditExpense from "./components/EditExpense";
 
 function App() {
+  console.log(process.env);
   return (
     <Auth0Provider
       domain={process.env.REACT_APP_AUTH0_DOMAIN}
